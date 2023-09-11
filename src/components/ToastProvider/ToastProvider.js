51ce6baf8915a7418688665015b95ad23c411e5d
hector alcazar <hiad99@gmail.com>
2023-09-11 19:17:17 +0200

    feat: scape keydown

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -5,6 +5,20 @@ export const ToastContext = createContext();
 function ToastProvider({ children }) {
   const [toastInformation, setToastInformation] = useState([]);
 
+  React.useEffect(() => {
+    function handleKeyDown(event) {
+      if (event.code === "Escape") {
+        setToasts([]);
+      }
+    }
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
   return (
     <ToastContext.Provider value={{ toastInformation, setToastInformation }}>
       {children}
